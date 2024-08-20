31efd40801e5c0ffe3983a5edd98604b1966a44b
arjun kshirsagar <kshirsagararjun20@gmail.com>
2024-08-20 14:50:35 +0530

    Extended the model with due_date field

--- todos/models.py
+++ todos/models.py
@@ -8,5 +8,7 @@ class Todo(models.Model):
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    due_date = models.DateField(null=True, blank=True)
+
     def __str__(self):
         return self.title
--- todos/serializers.py
+++ todos/serializers.py
@@ -4,4 +4,4 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'completed', 'created_at']
+        fields = ['id', 'title', 'description', 'completed', 'created_at', 'due_date']
